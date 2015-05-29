7a0cee86e956078b60c568887af455a66bd221c8
Yerzhan <yerzhan>
2015-05-29 19:28:55 +0600

    renamed creation script and added deletion script

--- db/create.sql
+++ db/create.sql
@@ -1,3 +1,4 @@
+-- psql -h localhost -U postgres
 create user ihlas password 'xxx';
 create user ihlas_web password 'xxx';
 create database ihlas owner ihlas;
---
+++ db/drop.sql
@@ -0,0 +1,4 @@
+-- psql -h localhost -U postgres
+drop database ihlas;
+drop role ihlas_web;
+drop role ihlas;
